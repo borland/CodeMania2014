8f3875e6150ca0810d67acd7cfc3f88a543cecc5
Orion Edwards <orion.edwards@gmail.com>
2014-03-30 17:34:04 +1300

    ready for demo 1

--- demo1/main.cpp
+++ demo1/main.cpp
@@ -5,6 +5,39 @@
 
 using namespace std;
 
+string safe_sprintf(string format, string arg) {
+    size_t pos = format.find("%@", 0);
+    assert(pos != string::npos);
+    
+    format.replace(pos, 2, arg);
+    return format;
+};
+
+int main(int argc, const char * argv[])
+{
+    string targ = safe_sprintf("hello %@, have a nice day!", "joe");
+    
+    printf("%s\n", targ.c_str());
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 template <typename T>
 struct StringConverter {
     static string toString(T item) {
@@ -22,23 +55,3 @@ struct StringConverter<T*> {
         return s.str();
     }
 };
-
-template<typename T>
-string safe_sprintf(string format, T arg) {
-    size_t pos = format.find("%@", 0);
-    assert(pos != string::npos);
-    
-    string argString = StringConverter<T>::toString(arg);
-    
-    format.replace(pos, 2, argString);
-    return format;
-};
-
-int main(int argc, const char * argv[])
-{
-    int a = 7;
-    string targ = safe_sprintf("hello %@, have a nice day!", "joe");
-    
-    printf("%s\n", targ.c_str());
-}
-
