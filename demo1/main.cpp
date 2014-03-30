93eb83bffde392d4d1336710c63cf48d9c4ee15e
Orion Edwards <orion.edwards@gmail.com>
2014-03-30 17:32:43 +1300

    demo 1 string converters done

--- demo1/main.cpp
+++ demo1/main.cpp
@@ -1,21 +1,43 @@
 #include <string>
+#include <sstream>
 #include <algorithm>
+#include <cassert>
 
 using namespace std;
 
-string safe_sprintf(string format, const string& arg1) {
-    size_t pos = 0;
-    while((pos = format.find("%@", pos)) != string::npos) {
-
-        format.replace(pos, 2, arg1);
-        pos += arg1.length();
+template <typename T>
+struct StringConverter {
+    static string toString(T item) {
+        ostringstream s;
+        s << item;
+        return s.str();
     }
+};
+
+template <typename T>
+struct StringConverter<T*> {
+    static string toString(T* item) {
+        ostringstream s;
+        s << *item;
+        return s.str();
+    }
+};
+
+template<typename T>
+string safe_sprintf(string format, T arg) {
+    size_t pos = format.find("%@", 0);
+    assert(pos != string::npos);
+    
+    string argString = StringConverter<T>::toString(arg);
+    
+    format.replace(pos, 2, argString);
     return format;
 };
 
 int main(int argc, const char * argv[])
 {
-    string targ = safe_sprintf("hello %@, have a %@ day!", "orion");
+    int a = 7;
+    string targ = safe_sprintf("hello %@, have a nice day!", "joe");
     
     printf("%s\n", targ.c_str());
 }
