e7e486f2462eb2c71d1fe0273b4001b508567bfe
Orion Edwards <orion.edwards@gmail.com>
2014-04-01 16:49:05 +1300

    start demo 1 with a template function already existing

--- demo1/main.cpp
+++ demo1/main.cpp
@@ -6,18 +6,26 @@
 
 using namespace std;
 
-string safe_sprintf(string format, string arg) {
+template<typename T>
+struct StringConverter {
+    static string convert(T item) {
+        ostringstream o; o << item; return o.str();
+    }
+};
+
+template<typename T>
+string safe_sprintf(string format, T arg) {
     size_t pos = format.find("%@", 0);
     assert(pos != string::npos);
     
-    format.replace(pos, 2, arg);
+    format.replace(pos, 2, StringConverter<T>::convert(arg));
     return format;
 };
 
 int main(int argc, const char * argv[])
 {
-    string person { "orion" };
-    string targ = safe_sprintf("hello %@ - have a nice day!", person);
+    string orion { "orion" };
+    string targ = safe_sprintf("hello %@ - have a nice day!", orion);
     
     printf("%s\n", targ.c_str());
 }
